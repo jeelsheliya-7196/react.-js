38b13fdf7348f8da3d7747a7339fb6eb12f56784
jeelsheliya-7196 <jeelsheliya71@gmail.com>
2023-05-08 10:22:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import images from './assets/images/download.jpg';
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='car'>
+        {/* <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a> */}
+        <img src={images} alt="images" />
+        <h3>Name:- <span>Jeel Sheliya</span></h3>
+        <h3>Phone:- <span>6353481047</span></h3>
+        <h3>Email:- <span>jeel71@gmail.com</span></h3>
+        {/* <h3>Name:- <span>Jeel Sheliya</span></h3> */}
+      </div>
+      {/* <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p> */}
+      
+    </>
+  )
+}
+
+export default App
