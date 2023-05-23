5b373c4b18d6deccceff8618d4ac2c8974ccb304
jeelsheliya-7196 <jeelsheliya71@gmail.com>
2023-05-23 10:16:47 +0530

    demo2

--- src/component/App/App.jsx
+++ src/component/App/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import images from './assets/images/download.jpg';
+import reactLogo from '../../assets/react.svg'
+import images from '../../assets/images/download.jpg';
 import viteLogo from '/vite.svg'
 import './App.css'
+import List from '../List/List'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,19 +11,20 @@ function App() {
   return (
     <>
       <div className='car'>
-        {/* <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a> */}
-        <img src={images} alt="images" />
+        </a>
+        {/* <img src={images} alt="images" />
         <h3>Name:- <span>Jeel Sheliya</span></h3>
         <h3>Phone:- <span>6353481047</span></h3>
-        <h3>Email:- <span>jeel71@gmail.com</span></h3>
+        <h3>Email:- <span>jeel71@gmail.com</span></h3> */}
         {/* <h3>Name:- <span>Jeel Sheliya</span></h3> */}
       </div>
-      {/* <h1>Vite + React</h1>
+      {/* <h1>Vite + React</h1> */}
+      < List />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -33,7 +35,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p> */}
+      </p>
       
     </>
   )
---
+++ src/component/List/List.jsx
@@ -0,0 +1,9 @@
+function List(){
+    return(
+        <div>
+            <h1>heeloo world</h1>
+        <h2> jeel sheliya</h2>
+        </div>
+    )
+}
+export default List;
